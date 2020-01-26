560045d79023a2bfd3770771eda8a0d8e9a3b74b
traderjeffb <traderjeffb@gmail.com>
2020-01-26 18:05:37 -0500

    still working on input needs attention

--- portfolio1/script.js
+++ portfolio1/script.js
@@ -1,37 +1,20 @@
 var stock;
 var notCleanInput;
-var input;
+var input = "GOOG";
 var api =
   "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
 var apiKey = "&apikey=VBVT9P94JCTSHO29"; //'VBVT9P94JCTSHO29'
 var url = api + input + apiKey;
-var data;
 
 function selectText() {
   const notCleanInput = document.getElementById("stock").value;
-  input = notCleanInput.replace(/\s+/g, "");
-  url = api + input + apiKey;
-
-  console.log("input is " + input);
-  console.log("url is " + url);
+  const input = notCleanInput.replace(/\s+/g, "");
+  return input;
 }
 
-// async function getQuote() {
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   console.log(data);
-// }
-//console.log(data);
-// console.log(url);
-function getQuote() {
-  fetch(url)
-    .then(res => {
-      if (res.ok) {
-        console.log("SUCCESS");
-      } else {
-        console.log("NOT SUCCESSFUL");
-      }
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log("ERROR"));
+async function getQuote() {
+  let response = await fetch(url);
+  console.log("here is " + url);
+  let data = await response.json();
+  console.log(data);
 }
