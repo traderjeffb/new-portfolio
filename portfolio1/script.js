7da1b14bb18642ecf539827ac3dbf46b3b64c8f4
traderjeffb <traderjeffb@gmail.com>
2020-01-29 21:57:35 -0500

    cleaned up js file, started working with data

--- portfolio1/script.js
+++ portfolio1/script.js
@@ -1,26 +1,4 @@
-// var stock;
-// var notCleanInput;
-// var input = "FB";
-// var api =
-//   "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
-// var apiKey = "&apikey=VBVT9P94JCTSHO29"; //'VBVT9P94JCTSHO29'
-// var url = api + input + apiKey;
-
-// function selectText() {
-//   const notCleanInput = document.getElementById("stock").value;
-//   const input = notCleanInput.replace(/\s+/g, "");
-//   console.log("this is input" + input);
-// }
-
-// async function getQuote() {
-//   let response = await fetch(url);
-//   console.log("here is " + url);
-//   let data = await response.json();
-//   console.log(data);
-// }
-
-//=================================================================
-var stock;
+var ticker;
 var notCleanInput;
 var input;
 var api =
@@ -29,16 +7,15 @@ var apiKey = "&apikey=VBVT9P94JCTSHO29"; //'VBVT9P94JCTSHO29'
 var url = api + input + apiKey;
 
 function selectText() {
-  const notCleanInput = document.getElementById("stock").value;
+  const notCleanInput = document.getElementById("ticker").value;
   input = notCleanInput.replace(/\s+/g, "");
-  //console.log("this is input " + input);
   url = api + input + apiKey;
-  //console.log("line 36 " + url);
 }
 
 async function getQuote() {
   let response = await fetch(url);
-  //console.log("here is " + url);
   let data = await response.json();
   console.log(data);
+  console.log(data.symbol); //trying to manipulate the meta data
+  //console.log(data.symbol);
 }
